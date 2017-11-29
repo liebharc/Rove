9f4bc2afdbcb83b7ebf32c621cad17a0f284cc37
Christian Liebhardt <christian.liebhardt@arcor.de>
2017-11-29 21:31:52 +0100

    Added some notes regarding configuration

---
+++ Rove/Model/Config.cs
@@ -0,0 +1,25 @@
+﻿using System.IO;
+using System.Text.RegularExpressions;
+
+namespace Rove.Model
+{
+    public class OverallConfig
+    {
+        public FileInfo OnNewProcessScript { get; set; }
+    }
+
+    public class ProcessConfig
+    {
+        string ProcessName { get; set; }
+
+        public Regex WarningMessage { get; set; }
+
+        public Regex ErrorMessage { get; set; }
+
+        public FileInfo OnProcessStartedScript { get; set; }
+
+        public FileInfo FindLogFileScript { get; set; }
+
+        public FileInfo IsKnownProcessScript { get; set; }
+    }
+}
