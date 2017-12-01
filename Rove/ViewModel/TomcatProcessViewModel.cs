b7f7612bb3e8e0e02f9eb270797668852f07817a
Christian Liebhardt <christian.liebhardt@arcor.de>
2017-12-01 20:18:43 +0100

    Refactoring

--- Rove/ViewModel/TomcatProcessViewModel.cs
+++ Rove/ViewModel/TomcatProcessViewModel.cs
@@ -1,7 +1,6 @@
 ﻿using Rove.Model;
 using System;
 using System.Collections.Generic;
-using System.Collections.ObjectModel;
 using System.ComponentModel;
 using System.IO;
 using System.Linq;
@@ -351,42 +350,6 @@ namespace Rove.ViewModel
             OnPropertyChanged(nameof(Tomcat));
             OnPropertyChanged(nameof(IsEnabled));
             OnPropertyChanged(nameof(IsVisible));
-        }
-    }
-
-    public sealed class TomcatProcessViewModelCollection : IDisposable
-    {
-        public ObservableCollection<TomcatProcessViewModel> Processes { get; } 
-            = new ObservableCollection<TomcatProcessViewModel>();
-
-        public TomcatProcessViewModelCollection(OverallConfig config)
-        {
-            if (config == null)
-            {
-                throw new ArgumentNullException(nameof(config));
-            }
-            
-            foreach (var process in config.ProcessConfigs)
-            {
-                Processes.Add(new TomcatProcessViewModel(config, process));
-            }
-        }
-
-        public void Dispose()
-        {
-            foreach (var tomcat in Processes)
-            {
-                tomcat.Dispose();
-            }
-        }
-
-        internal void Update()
-        {
-            var tomcats = TomcatProcessInfo.RunningTomcatProcesses;
-            foreach (var process in Processes)
-            {
-                process.Update(tomcats);
-            }
         }
     }
 }
---
+++ Rove/ViewModel/TomcatProcessViewModelCollection.cs
@@ -0,0 +1,42 @@
+﻿using Rove.Model;
+using System;
+using System.Collections.ObjectModel;
+
+namespace Rove.ViewModel
+{
+    public sealed class TomcatProcessViewModelCollection : IDisposable
+    {
+        public ObservableCollection<TomcatProcessViewModel> Processes { get; } 
+            = new ObservableCollection<TomcatProcessViewModel>();
+
+        public TomcatProcessViewModelCollection(OverallConfig config)
+        {
+            if (config == null)
+            {
+                throw new ArgumentNullException(nameof(config));
+            }
+            
+            foreach (var process in config.ProcessConfigs)
+            {
+                Processes.Add(new TomcatProcessViewModel(config, process));
+            }
+        }
+
+        public void Dispose()
+        {
+            foreach (var tomcat in Processes)
+            {
+                tomcat.Dispose();
+            }
+        }
+
+        internal void Update()
+        {
+            var tomcats = TomcatProcessInfo.RunningTomcatProcesses;
+            foreach (var process in Processes)
+            {
+                process.Update(tomcats);
+            }
+        }
+    }
+}
