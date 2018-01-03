9be20addcfcc9887cec6f396058de24dd4218390
Christian Liebhardt <christian.liebhardt@arcor.de>
2018-01-03 20:58:29 +0100

    Move ProcessExtensions in an own file

---
+++ Rove/Model/ProcessExtensions.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Diagnostics;
+
+namespace Rove.Model
+{
+    internal static class ProcessExtensions
+    {
+        private static string FindIndexedProcessName(int pid)
+        {
+            var processName = Process.GetProcessById(pid).ProcessName;
+            var processesByName = Process.GetProcessesByName(processName);
+            string processIndexedName = null;
+            for (int index = 0; index < processesByName.Length; index++)
+            {
+                processIndexedName = index == 0 ? processName : processName + "#" + index;
+                var processId = new PerformanceCounter("Process", "ID Process", processIndexedName);
+                if ((int)processId.NextValue() == pid)
+                {
+                    return processIndexedName;
+                }
+            }
+
+            return processIndexedName;
+        }
+
+        private static Process FindPidFromIndexedProcessName(string indexedProcessName)
+        {
+            var parentId = new PerformanceCounter("Process", "Creating Process ID", indexedProcessName);
+            try
+            {
+                return Process.GetProcessById((int)parentId.NextValue());
+            }
+            catch (ArgumentException)
+            {
+                return null;
+            }
+            catch (InvalidOperationException)
+            {
+                return null;
+            }
+        }
+
+        public static Process Parent(this Process process)
+        {
+            if (process == null)
+            {
+                throw new ArgumentNullException(nameof(process));
+            }
+
+            return FindPidFromIndexedProcessName(FindIndexedProcessName(process.Id));
+        }
+    }
+}
