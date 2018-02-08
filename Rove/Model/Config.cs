19de485fb944787451e66cf9a76a0c91cdf5263e
Christian Liebhardt <christian.liebhardt@arcor.de>
2018-02-08 19:06:34 +0100

    Moved class

--- Rove/Model/Config.cs
+++ Rove/Model/Config.cs
@@ -7,18 +7,6 @@ using System.Xml.Serialization;
 
 namespace Rove.Model
 {
-    public class ConfigException : Exception
-    {
-        public ConfigException(string section, string value, Exception innerException = null) : base("Configuration failure in section " + section + " for value " + value, innerException)
-        {
-            Section = section;
-            Value = value;
-        }
-
-        public string Section { get; }
-        public string Value { get; }
-    }
-
     public class OverallConfig
     {
         public static OverallConfig DefaultConfig { 
---
+++ Rove/Model/ConfigException.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace Rove.Model
+{
+    public class ConfigException : Exception
+    {
+        public ConfigException(string section, string value, Exception innerException = null) : base("Configuration failure in section " + section + " for value " + value, innerException)
+        {
+            Section = section;
+            Value = value;
+        }
+
+        public string Section { get; }
+        public string Value { get; }
+    }
+}
